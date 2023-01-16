7ce81a3737f2e422fa9e531550ebab9133e8ce56
Breno Lins <36646169+BrenoLins100@users.noreply.github.com>
2023-01-16 19:38:26 -0300

    Headless: true

--- src/config/options.js
+++ src/config/options.js
@@ -7,7 +7,7 @@ const options = (start)=>{
         authTimeout: 60, //wait only 60 seconds to get a connection with the host account device
         blockCrashLogs: true,
         disableSpins: true,
-        headless: false,
+        headless: true,
         hostNotificationLang: 'PT_BR',
         logConsole: false,
         popup: true,
